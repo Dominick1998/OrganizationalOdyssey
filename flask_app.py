4597f2cec701bce45d4a724d0321a62e466edcc0
RyanEbsen <77689652+RyanEbsen@users.noreply.github.com>
2023-10-27 21:24:34 -0500

    Added login and registration functinality

--- flask_app.py
+++ flask_app.py
@@ -1,39 +1,116 @@
+import os
+
 from flask import Flask, render_template, url_for, redirect, request, flash
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask_mail import Mail, Message
+from cryptography.fernet import Fernet
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "c6d2f9789a32a64e8d12d42d2c955505"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config['MAIL_SERVER'] = "smtp.gmail.com."
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = "organizationalodyssey@gmail.com"
+app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config["FERNET_KEY"] = os.environ.get("FERNET_KEY")
+#app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://RyanEbsen:+$VZKM0^w)7;j@RyanEbsen.mysql.pythonanywhere-services.com/RyanEbsen$default"
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+mail = Mail(app)
+fernet = Fernet(app.config["FERNET_KEY"])
+login_manager = LoginManager(app)
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+    email_confirmed = db.Column(db.Boolean, nullable=False, default=False)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 @app.route('/')
-def home():  # put application's code here
-    return render_template("home.html")
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if request.method == "POST" and form.validate():
+        user = User.query.filter_by(email=form.email.data).first()
+        if not user:
+            flash(f"No account exists with that email", "danger")
+            return redirect(url_for("login"))
+        if not user.email_confirmed:
+            flash(f"Please activate your account before loging in", "danger")
+            return redirect(url_for("login"))
+        if bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect(url_for("home"))
+        else:
+            flash(f"invalid credentials")
+            return redirect(url_for("login"))
+    return render_template("login.html", title="Log in", form=form)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
-    if request.method == 'POST' and form.validate():
-        flash(f"Account successfully created", "success")
-        return redirect(url_for("home"))
+    if request.method == "POST" and form.validate():
+        email = User.query.filter_by(email=form.email.data).first()
+        if email:
+            flash(f"An account with that email already exists", "danger")
+            return redirect(url_for("register"))
+        encrypted_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(email=form.email.data, password=encrypted_password)
+        db.session.add(user)
+        db.session.commit()
+
+        token = fernet.encrypt(user.email.encode())
+        confirm_url = url_for("confirm_account", token=token, _external=True)
+        html = render_template("email.html", confirm_url=confirm_url)
+        msg = Message(
+            "Confirm your email with Organizational Odyssey!",
+            recipients=[user.email],
+            html=html,
+            sender="organizationalodyssey@gmail.com"
+        )
+        mail.send(msg)
+
+        flash(f"Thank you for signing up! Please check your email to confirm your account", "success")
+        return redirect(url_for("login"))
     return render_template("register.html", title="Registration", form=form)
 
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
-    return render_template("login.html", title="Log in", form=form)
+@app.route("/home")
+@login_required
+def home():  # put application's code here
+    return render_template("home.html")
+
+
+@app.route("/confirm/<token>")
+def confirm_account(token):
+    email = fernet.decrypt(token).decode()
+    user = User.query.filter_by(email=email).first()
+    user.email_confirmed = True
+    db.session.commit()
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
