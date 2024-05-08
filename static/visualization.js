53defd31b888f55bb4c95b1d81d9826956661f50
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2024-05-08 11:31:06 -0500

    Update visualization.js
    proper visualization

--- static/visualization.js
+++ static/visualization.js
@@ -7,28 +7,22 @@ function createChart(data, containerId) {
         chart.nodes().labels().format("{%name}");  // Ensure label is displayed
 
         chart.tooltip().useHtml(true);
-        var nodeFormat = getNodeFormat(data.nodes[0]);  // Determine format based on the first node's type
-        chart.nodes().tooltip().format(nodeFormat);
-        chart.edges().tooltip().format("From: {%from_name} </br> To: {%to_name}");
+
+        // Construct the format string with conditional display
+        chart.nodes().tooltip().format(
+            "Name: {%name}" +
+            "{%if address%} <br> Address: {%address} {%end if%}" +
+            "{%if location%} {%location} {%end if%}" +
+            "{%if start_date%} <br> Start Date: {%start_date} {%end if%}" +
+            "{%if end_date%} <br> End Date: {%end_date} {%end if%}" +
+            "{%if description%} <br> Description: {%description} {%end if%}" +
+            "{%if email%} <br> Email: {%email} {%end if%}" +
+            "{%if phone_number%} <br> Phone: {%phone_number} {%end if%}"
+        );
+
+        chart.edges().tooltip().format("From: {%from_name} <br> To: {%to_name}");
 
         chart.container(containerId);
         chart.draw();
     });
-}
-
-function getNodeFormat(node) {
-    let formatString = "Name: {%name}";
-
-    if (node.type === 'employer') {
-        formatString += " </br> Address: {%address}" +
-                        "</br> Start Date: {%start_date} </br> End Date: {%end_date}" +
-                        "</br> Description: {%description}";
-    } else if (node.type === 'employee') {
-        formatString += " </br> Email: {%email}" +
-                        "</br> Phone Number: {%phone_number}";
-    } else if (node.type === 'institution') {
-        formatString += " </br> Location: {%location}" +
-                        "</br> Description: {%description}";
-    }
-    return formatString;
 }
